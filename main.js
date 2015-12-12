a2c933aeb317e11412f70899b23b7aad38720570
Tzu-Te Yu <tedyu002@gmail.com>
2015-12-12 20:52:35 +0800

    Handle socket write error. Chanes socket closing from end(half) to destory(full).

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ else {
 		var readline_inst;
 		console.log(log_prefix + 'client connected');
 
-		connection.on('end', function() {
+		connection.on('close', function() {
 			readline_inst.emitter.removeAllListeners();
 			connection.removeAllListeners();
 			console.log(log_prefix + 'cliet disconnected');
@@ -81,9 +81,24 @@ else {
 
 		readline_inst = readline.instance(connection, 'evt_cmd', 'evt_data', 'evt_data_end', 'evt_char_invalid', 'buf_overflow_event');
 
+		connection.on('error', function() {
+			console.log(log_prefix + ' connection error');
+			connection.destroy();
+		});
+
+		var safe_send = function(mesg, callback) {
+			try{
+				connection.write(mesg, callback);
+			}
+			catch(e) {
+				console.log(log_prefix + e);
+				connection.destroy();
+			}
+		};
+
 		var next_cmd = function() {
 			readline_inst.read_next();
-		}
+		};
 
 		connection.on('data', readline_inst.read);
 		connection.pause();
@@ -112,59 +127,59 @@ else {
 				case 'HELO':
 					if (domain.type === 'domain') {
 						if (res.is_ext === 1) {
-							connection.write("250-" + config.domain_name + " greeting " + domain.value + "\r\n" +
-											 "250 SIZE " + config.mail_data_max + "\r\n", next_cmd);
+							safe_send("250-" + config.domain_name + " greeting " + domain.value + "\r\n" +
+									  "250 SIZE " + config.mail_data_max + "\r\n", next_cmd);
 						}
 						else {
-							connection.write("250 " + config.domain_name + " greeting " + domain.value + "\r\n", next_cmd);
+							safe_send("250 " + config.domain_name + " greeting " + domain.value + "\r\n", next_cmd);
 						}
 						mail_transaction = new MailTransaction();
 					}
 					else {
-						connection.write("550 syntax error domain name '" + res.args + "'\r\n", next_cmd);
+						safe_send("550 syntax error domain name '" + res.args + "'\r\n", next_cmd);
 					}
 					break;
 				case 'MAIL':
 					if (mail_transaction.mail_from != null) {
-						connection.write('503 Mail Already be set\r\n', next_cmd);
+						safe_send('503 Mail Already be set\r\n', next_cmd);
 					}
 					else {
 						if (domain.type === 'path') {
 							mail_transaction.mail_from = domain;
-							connection.write("250 The reverse-path is '" + domain.value.local_part + '@' + domain.value.domain + "'\r\n", next_cmd);
+							safe_send("250 The reverse-path is '" + domain.value.local_part + '@' + domain.value.domain + "'\r\n", next_cmd);
 						}
 						else if (domain.type === 'empty') {
 							mail_transaction.mail_from = domain;
-							connection.write("250 The reverse-path is empty\r\n", next_cmd);
+							safe_send("250 The reverse-path is empty\r\n", next_cmd);
 						}
 						else if (domain.type === 'domain') {
-							connection.write("553 '" + domain.value + "' is a domain, not a mailbox\r\n", next_cmd);
+							safe_send("553 '" + domain.value + "' is a domain, not a mailbox\r\n", next_cmd);
 						}
 						else {
-							connection.write("553 '" + res.args + "' is not a mailbox.\r\n", next_cmd);
+							safe_send("553 '" + res.args + "' is not a mailbox.\r\n", next_cmd);
 						}
 					}
 					break;
 				case 'RCPT':
 					if (mail_transaction.mail_from == null) {
-						connection.write('503 Mail from is not set\r\n', next_cmd);
+						safe_send('503 Mail from is not set\r\n', next_cmd);
 					}
 					else if (mail_transaction.rcpt.length >= config.rcpt_max) {
-						connection.write('452 Too many recipients\r\n', next_cmd);
+						safe_send('452 Too many recipients\r\n', next_cmd);
 					}
 					else {
 						if (domain.type === 'path') {
 							mail_transaction.rcpt.push(domain);
-							connection.write("250 The forward-path is '" + domain.value.local_part + '@' + domain.value.domain + "'\r\n", next_cmd);
+							safe_send("250 The forward-path is '" + domain.value.local_part + '@' + domain.value.domain + "'\r\n", next_cmd);
 						}
 						else if (domain.type === 'domain') {
-							connection.write("553 '" + domain.value + "' is a domain, not a mailbox\r\n", next_cmd);
+							safe_send("553 '" + domain.value + "' is a domain, not a mailbox\r\n", next_cmd);
 						}
 						else if (domain.type === 'empty') {
-							connection.write("553 mailbox can't be empty\r\n", next_cmd);
+							safe_send("553 mailbox can't be empty\r\n", next_cmd);
 						}
 						else {
-							connection.write("553 '" + res.args + "' is not a mailbox.\r\n", next_cmd);
+							safe_send("553 '" + res.args + "' is not a mailbox.\r\n", next_cmd);
 						}
 					}
 					break;
@@ -172,28 +187,28 @@ else {
 					if (mail_transaction.all_set()) {
 						if (mail_transaction.open()) {
 							readline_inst.enter_data_mode();
-							connection.write("354 Start mail input; end with <CRLF>.<CRLF>\r\n", next_cmd);
+							safe_send("354 Start mail input; end with <CRLF>.<CRLF>\r\n", next_cmd);
 						}
 						else {
 							mail_transaction = new MailTransaction();
-							connection.write("452 insufficient system storage\r\n", next_cmd);
+							safe_send("452 insufficient system storage\r\n", next_cmd);
 						}
 					}
 					else {
-						connection.write("503 Reverse path or forward-path not set\r\n", next_cmd);
+						safe_send("503 Reverse path or forward-path not set\r\n", next_cmd);
 					}
 					break;
 				case 'RSET':
 					mail_transaction = new MailTransaction();
-					connection.write('250\r\n', next_cmd);
+					safe_send('250\r\n', next_cmd);
 					break;
 				case 'QUIT':
-					connection.write('221\r\n', function() {
-						connection.end();
+					safe_send('221\r\n', function() {
+						connection.destroy();
 					});
 					break;
 				default:
-					connection.write("550 '" + cmd_line + "' not recognize\r\n", next_cmd);
+					safe_send("550 '" + cmd_line + "' not recognize\r\n", next_cmd);
 					break;
 			}
 		});
@@ -220,7 +235,7 @@ else {
 
 			mail_transaction.finish(failed);
 
-			connection.write(message, function() {
+			safe_send(message, function() {
 				mail_transaction = new MailTransaction();
 				readline_inst.disable_data_mode();
 				readline_inst.read_next();
@@ -228,20 +243,20 @@ else {
 		});
 
 		readline_inst.emitter.on('evt_char_invalid', function() {
-			connection.write('500 syntax error - invalid character\r\n', function() {
+			safe_send('500 syntax error - invalid character\r\n', function() {
 				readline_inst.read_next();
 			});
 		});
 
 		readline_inst.emitter.on('buf_overflow_event', function() {
-			connection.write('500 syntax error - the line to long\r\n', function() {
+			safe_send('500 syntax error - the line to long\r\n', function() {
 				readline_inst.disable_data_mode();
 				readline_inst.read_next();
 			});
 		});
 
 		connection.on('timeout', function() {
-			connection.end();
+			connection.destroy();
 		});
 
 		connection.setTimeout(config.idle_time);
